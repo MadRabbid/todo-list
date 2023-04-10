d1393549703c956a0d64f256c183ca452779b441
Artem Zaitsev <maaad.rabbit@gmail.com>
2023-04-10 23:38:49 +0200

    Code refactor

--- todo.js
+++ todo.js
@@ -1,91 +1,74 @@
-let taskList;
+(() => {
+    let taskList = JSON.parse(localStorage.getItem('taskList')) ?? [];
 
-document.addEventListener("DOMContentLoaded", function () {
-    if (localStorage.getItem('taskList') === null) {
-        taskList = [];
-    } else {
-        taskList = JSON.parse(localStorage.getItem('taskList'));
-    }
-});
-
-function createTask(taskContent) {
-    let task = new Object();
-    task.content = taskContent;
-    task.done = false;
-    taskList.push(task);
-    saveTaskList();
-}
-
-function showTasks(filter) {
-    if (filter === undefined) {
-        for (let i = 0; i < taskList.length; i++) {
-            if (!taskList[i].done) {
-                console.log(`[ ] ${i}. ${taskList[i].content}`);
-            } else {
-                console.log(`[x] ${i}. ${taskList[i].content}`);
-            }
+    const createTask = (content) => {
+        let task = {
+            content: content,
+            done: false,
         }
-    } else if (filter === 'done') {
-        for (let i = 0; i < taskList.length; i++) {
-            if (taskList[i].done) {
-                console.log(`[x] ${i}. ${taskList[i].content}`);
-            }
-        }
-    } else if (filter === 'undone') {
-        for (let i = 0; i < taskList.length; i++) {
-            if (!taskList[i].done) {
-                console.log(`[ ] ${i}. ${taskList[i].content}`);
-            }
-        }
-    } else {
-        showTasks();
-    }
-
-}
-
-function deleteTask(taskId) {
-    try {
-        taskList.splice(taskId, 1);
-        showTasks();
+        taskList.push(task);
         saveTaskList();
-    } catch {
-        console.error('There is no task with these id.');
-        showTasks();
     }
 
-}
-
-function markTask(taskId, removeTask = false) {
-    try {
-        if (!taskList[taskId].done) {
-            taskList[taskId].done = true;
-        } else {
-            taskList[taskId].done = false;
+    function showTasks(filter) {
+        for (let i = 0; i < taskList.length; i++) {
+            let text = `${i}. ${taskList[i].content}`;
+            let listItem = taskList[i].done ? '[x] ' + text : '[ ] ' + text;
+            if (filter === undefined) {
+                console.log(listItem);
+            } else if (filter === 'done') {
+                if (taskList[i].done) {
+                    console.log(listItem);
+                }
+            } else if (filter === 'undone') {
+                if (!taskList[i].done) {
+                    console.log(listItem);
+                }
+            }
         }
+        saveTaskList();
+    }
 
-
-        if (removeTask === true) {
-            deleteTask(taskId);
+    function deleteTask(id) {
+        try {
+            taskList.splice(id, 1);
             saveTaskList();
-        } else {
-            showTasks();
-            saveTaskList();
+        } catch {
+            console.error('There is no task with these id.');
         }
-    } catch {
-        console.error('There is no task with these id.');
-        showTasks();
     }
-}
 
-function saveTaskList() {
-    localStorage.setItem('taskList', JSON.stringify(taskList));
-}
-
-function clearTaskList() {
-    try {
-        localStorage.removeItem('taskList');
-    } catch {
-        console.error('There is no saved data.');
+    function markTask(id) {
+        let taskStatus = taskList[id].done;
+        try {
+            taskStatus ?
+                taskStatus = false :
+                taskStatus = true;
+            saveTaskList();
+        } catch {
+            console.error('There is no task with these id.');
+        }
+        
     }
-    taskList = [];
-}
+
+    function saveTaskList() {
+        localStorage.setItem('taskList', JSON.stringify(taskList));
+    }
+
+    function clearTaskList() {
+        try {
+            localStorage.removeItem('taskList');
+        } catch {
+            console.error('There is no saved data.');
+        }
+        taskList = [];
+    }
+
+    window.todo = {
+        createTask: createTask,
+        showTasks: showTasks,
+        deleteTask: deleteTask,
+        markTask: markTask,
+        clearTaskList: clearTaskList,
+    }
+})();
